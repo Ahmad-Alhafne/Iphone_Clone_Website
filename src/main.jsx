45ce28a58d80e88b26ed534941209d3e23a999e7
Ahmad-Alhafne <ahmad4732916@gmail.com>
2025-03-09 18:08:15 +0300

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+//...
+import * as Sentry from "@sentry/react";
+
+Sentry.init({ 
+  dsn: "https://cbd1264e9660c16e2d25439ab07157d1@o4507956338753536.ingest.us.sentry.io/4507956347863040",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.metrics.metricsAggregatorIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  tracesSampleRate: 1.0, 
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0, 
+});
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
